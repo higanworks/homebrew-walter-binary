3c14c52406291607394d1e338a1538a2c48d9570
sawanoboly <sawanoboriyu@higanworks.com>
2014-12-17 23:07:48 +0900

    upgrade version to 0.2.0

--- walter.rb
+++ walter.rb
@@ -1,16 +1,16 @@
 require 'formula'
 class Walter < Formula
   homepage 'http://ainoya.io/walter'
-  version '0.1.0'
+  version '0.2.0'
 
   depends_on :arch => :intel
 
   if Hardware.is_64_bit?
-    url 'https://github.com/walter-cd/walter/releases/download/v0.1.0/walter_darwin_amd64.zip'
-    sha256 'ebe31bfea6f298576fc8a6169e94374012d49039e7823f0e4f998d1e2305feb6'
+    url 'https://github.com/walter-cd/walter/releases/download/v0.2.0/walter_darwin_amd64.zip'
+    sha256 '664cbab26e556d2da737de7ca384a40bf358ecd02898e7aa5d0c1393f711b55b'
   else
-    url 'https://github.com/walter-cd/walter/releases/download/v0.1.0/walter_darwin_386.zip'
-    sha256 'e200eb30c06269b1781925def75054db39aa029b39bb6255078012530a1e5af7'
+    url 'https://github.com/walter-cd/walter/releases/download/v0.2.0/walter_darwin_386.zip'
+    sha256 '17e73d129582ae0309509a2fdda4437b82900436b60d7fae22ebd4349f74ab3c'
   end
 
   def install
