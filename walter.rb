56b0cde60002c5cb355f9eb96b4cc6ca2de08b4e
sawanoboly <sawanoboriyu@higanworks.com>
2015-08-04 17:16:24 +0900

    update url

--- walter.rb
+++ walter.rb
@@ -6,10 +6,10 @@ class Walter < Formula
   depends_on :arch => :intel
 
   if Hardware.is_64_bit?
-    url 'https://github.com/walter-cd/walter/releases/download/v1.2.0/walter_darwin_amd64.zip'
+    url 'https://github.com/walter-cd/walter/releases/download/v1.2.0/walter_1.2.0_darwin_amd64.zip'
     sha256 '0bcea802750b2c547975c7303c4743056bae15c225aca307d08dbf27580475b5'
   else
-    url 'https://github.com/walter-cd/walter/releases/download/v1.2.0/walter_darwin_386.zip'
+    url 'https://github.com/walter-cd/walter/releases/download/v1.2.0/walter_1.2.0_darwin_386.zip'
     sha256 'c47cd0ea3ef520d6bec95e6c4ad32d56ab29c5a4ac5ebabbfa1d78ef2f7fd2e1'
   end
 
