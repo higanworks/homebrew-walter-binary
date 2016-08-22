0560a122915cd8d86e55f13fd6c2fe7b44f3795a
sawanoboly <sawanoboriyu@higanworks.com>
2016-08-22 13:30:52 +0900

    Use Hardware::CPU.is_64_bit?

--- walter.rb
+++ walter.rb
@@ -1,11 +1,11 @@
 require 'formula'
 class Walter < Formula
   homepage 'http://ainoya.io/walter'
-  version '1.4.0-1'
+  version '1.4.0-2'
 
   depends_on :arch => :intel
 
-  if Hardware.is_64_bit?
+  if Hardware::CPU.is_64_bit?
     url 'https://github.com/walter-cd/walter/releases/download/v1.4.0/walter_1.4.0_darwin_amd64.zip'
     sha256 '29c03d6ff2563227947de18cd1cabba8085e9e763a50982bdc7aabc49f1f0fc0'
   else
